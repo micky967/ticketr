cf9625af9be3b74ba6dff31e9cb980ec5a15b8e6
Manjit Lider <micky967@yahoo.com>
2024-12-12 01:35:14 -0800

    updated with the right upload image path graceful-basilisk-414.convex.cloud

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { hostname: "striped-grouse-591.convex.cloud", protocol: "https"},
-      { hostname: "dashboard.convex.dev/t/micky967/ticketr-9eadf/graceful-basilisk-414/files", protocol: "https"},
+      { hostname: "graceful-basilisk-414.convex.cloud", protocol: "https"},
     ],
   },
     eslint: {
